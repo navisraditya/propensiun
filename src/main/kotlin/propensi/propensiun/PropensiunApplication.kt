9e5d73529bbc486025d38d96fdcda59667a0582e
navisraditya <navisraditya@gmail.com>
2024-09-18 16:51:03 +0700

    init commit

---
+++ src/main/kotlin/propensi/propensiun/PropensiunApplication.kt
@@ -0,0 +1,11 @@
+package propensi.propensiun
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PropensiunApplication
+
+fun main(args: Array<String>) {
+	runApplication<PropensiunApplication>(*args)
+}
