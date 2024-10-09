1b89ed2915a4c2d3d5b9eafe4ea567d046601323
navisraditya <navisraditya@gmail.com>
2024-10-09 14:43:04 +0700

    update

--- src/main/java/propensi/propensiun/abuya/AbuyaApplication.java
+++ src/main/java/propensi/propensiun/abuya/AbuyaApplication.java
@@ -2,10 +2,8 @@ package propensi.propensiun.abuya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("propensi.propensiun.abuya.repository")
 public class AbuyaApplication {
 
 	public static void main(String[] args) {
