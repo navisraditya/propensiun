57cf594985f3ab66b20af78d4b93d2b35cee8abe
navisraditya <navisraditya@gmail.com>
2024-10-11 21:36:35 +0700

    i forgot to uncomment testing :D

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
-	// jacoco
+	jacoco
 }
 
 group = "propensi.propensiun"
@@ -47,20 +47,19 @@ dependencies {
 	implementation ("org.postgresql:postgresql")
 }
 
-//tasks.withType<Test> {
-//	useJUnitPlatform()
-//	finalizedBy(tasks.jacocoTestReport)
-//
-//	task.jacocoTestReport{
-//		classDirectories.setFrom(files(classDirectories.files.map {
-//			fileTree(it) { exclude("**/*Application**")}
-//		}))
-//		dependsOn(tasks.test)
-//		reports {
-//			xml.required.set(false)
-//			csv.required.set(false)
-//			html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
-//		}
-//	}
-//}
+tasks.withType<Test> {
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
 
+tasks.jacocoTestReport{
+	classDirectories.setFrom(files(classDirectories.files.map {
+		fileTree(it) { exclude("**/*Application**")}		}))
+	dependsOn(tasks.test)
+	reports {
+		
+		xml.required.set(false)
+		csv.required.set(false)
+		
+		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))		}
+}
